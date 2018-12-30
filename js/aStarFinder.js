564d225b3eb41a7d7adfc541069b0a5d70f5e511
Michał Terczyński <mterczynski271@gmail.com>
2018-12-30 22:53:56 +0100

    code styling

--- js/aStarFinder.js
+++ js/aStarFinder.js
@@ -1,144 +1,134 @@
-function AStarFinder(){
+function AStarFinder() {
     // Manhattan heuristic:
-    function getEstimate(from, to){  
+    function getEstimate(from, to) {
         return Math.abs(from.x - to.x) + Math.abs(from.y - to.y);
     }
 
-    function getTileScore(tile){
+    function getTileScore(tile) {
         return tile.estimate + tile.travelCost;
     }
     // return array of tiles sorted by their score
-    function getFreeAdjacentTiles(currentPos, board, currentTravelCost, boardSize, targetPos){
+    function getFreeAdjacentTiles(currentPos, board, currentTravelCost, boardSize, targetPos) {
         const adjacentTiles = [];
-        for(let x=-1; x<=1; x++){
-            for(let y=-1; y<=1; y++){
-                if((x != 0 || y != 0) && Math.abs(x%2) != Math.abs(y%2)){ // no self-pos and no diagonal tiles
-                    let tilePos = {x: currentPos.x + x, y: currentPos.y + y};
-                    let posExists = tilePos.x >=0 && tilePos.x < boardSize.x && tilePos.y >=0 && tilePos.y < boardSize.y;
-                    if(posExists){
+        for (let x = -1; x <= 1; x++) {
+            for (let y = -1; y <= 1; y++) {
+                if ((x != 0 || y != 0) && Math.abs(x % 2) != Math.abs(y % 2)) { // no self-pos and no diagonal tiles
+                    let tilePos = { x: currentPos.x + x, y: currentPos.y + y };
+                    let posExists = tilePos.x >= 0 && tilePos.x < boardSize.x && tilePos.y >= 0 && tilePos.y < boardSize.y;
+                    if (posExists) {
                         let adjacentTileValue = board[tilePos.x][tilePos.y];
-                        if(adjacentTileValue == null){
+                        if (adjacentTileValue == null) {
                             adjacentTiles.push({
                                 x: tilePos.x,
                                 y: tilePos.y,
                                 estimate: getEstimate(tilePos, targetPos),
                                 travelCost: currentTravelCost + 1
-                            });                           
+                            });
                         }
-                    }           
+                    }
                 }
             }
         }
-        adjacentTiles.sort((a,b)=>{
+        adjacentTiles.sort((a, b) => {
             return getTileScore(a) > getTileScore(b);
         });
         return adjacentTiles;
     }
 
-    function getAdjacentFromClosedList(closedList, currentPos){
-        return closedList.filter((tile)=>{
+    function getAdjacentFromClosedList(closedList, currentPos) {
+        return closedList.filter((tile) => {
             return Math.abs(tile.x - currentPos.x) == 1 && Math.abs(tile.y - currentPos.y) == 0 ||
-            Math.abs(tile.x - currentPos.x) == 0 && Math.abs(tile.y - currentPos.y) == 1;
+                Math.abs(tile.x - currentPos.x) == 0 && Math.abs(tile.y - currentPos.y) == 1;
         });
     }
 
-    function addUniquePointToArray(point, array){
-        if(array.filter((el)=>{
+    function addUniquePointToArray(point, array) {
+        if (array.filter((el) => {
             return el.x == point.x && el.y == point.y;
-        }).length == 0){
+        }).length == 0) {
             array.push(point);
             return true;
-        } 
+        }
         return false;
     }
 
-    function checkIfPointInList(point, list){
-        if(list.filter((el)=>{
+    function checkIfPointInList(point, list) {
+        if (list.filter((el) => {
             return el.x == point.x && point.y == el.y;
-        }).length > 0){
+        }).length > 0) {
             return true;
         } else {
             return false;
         }
     }
 
-    this.findPath = (array, from, targetPos, arraySize) =>{   
-        if(from.x == targetPos.x && from.y == targetPos.y){
-            return {success: false, openList:[], closedList:[]};
+    this.findPath = (array, from, targetPos, arraySize) => {
+        if (from.x == targetPos.x && from.y == targetPos.y) {
+            return { success: false, openList: [], closedList: [] };
         }
 
-        let openList = [];    
-        let currentPos = {x:from.x, y:from.y, travelCost: 0};
+        let openList = [];
+        let currentPos = { x: from.x, y: from.y, travelCost: 0 };
         const closedList = [currentPos];
-        
+
         // add adjacent tiles to open list:
         const adjacentTiles = getFreeAdjacentTiles(currentPos, array, 0, arraySize, targetPos);
-        adjacentTiles.forEach((el)=>{ 
-            openList.push(el); 
-        }); 
-        
-        while(!openList.length == 0){
+        adjacentTiles.forEach((el) => {
+            openList.push(el);
+        });
+
+        while (!openList.length == 0) {
             // 1. if openList is empty -> no more field to check -> no path
-            
+
             // 2. get the best tile from open list
-            let bestTileFromOpenList = openList.sort((a,b)=>{
+            let bestTileFromOpenList = openList.sort((a, b) => {
                 return getTileScore(a) - getTileScore(b);
             })[0];
             // 3. change position to the best tile and move it from openList to closedList
             currentPos = bestTileFromOpenList;
             closedList.push(openList.shift());
             // 4. if currentPos == targetPos compute final path:
-            if(currentPos.x == targetPos.x && currentPos.y == targetPos.y){
+            if (currentPos.x == targetPos.x && currentPos.y == targetPos.y) {
                 // compute and return final path:
                 const finalPath = [currentPos];
 
-                let iters = 1;
-                while(!(currentPos.x == from.x && currentPos.y == from.y)){
-                    iters++;
-
+                while (!(currentPos.x == from.x && currentPos.y == from.y)) {
                     const adjacentTiles = getAdjacentFromClosedList(closedList, currentPos);
 
-                    const someResult = adjacentTiles.some((tile)=>{
-                        if(currentPos.travelCost - 1 == tile.travelCost){
+                    adjacentTiles.some((tile) => {
+                        if (currentPos.travelCost - 1 == tile.travelCost) {
                             finalPath.push(tile);
                             currentPos = tile;
                             return true;
                         }
                     });
-
-                    if(!someResult){     
-                        console.log('closedList', closedList)
-                        console.warn('There is something wrong with A* algorithm');
-                        throw new Error('A* error');
-                    }
                 }
 
-                return {success: true, openList, closedList, finalPath};
+                return { success: true, openList, closedList, finalPath };
             }
             // 5. get adjacent tiles
             const adjacentTiles = getFreeAdjacentTiles(currentPos, array, currentPos.travelCost, arraySize, targetPos);
             // 6. forEach adjTile do action depending on in which list is the tile
-            adjacentTiles.forEach((adjTile)=>{  
+            adjacentTiles.forEach((adjTile) => {
                 // 6.1. if tile is in the closed list, ignore it
-                if(checkIfPointInList(adjTile, closedList)){
+                if (checkIfPointInList(adjTile, closedList)) {
                     // do nothing
-                } 
+                }
                 // 6.2. if tile is not in the open list: add it to open list.
-                else if(!checkIfPointInList(adjTile, openList)){
+                else if (!checkIfPointInList(adjTile, openList)) {
                     addUniquePointToArray(adjTile, openList);
                 }
                 // 6.3. if title is in the open list: 
                 else {
                     // Check if the travelCost is lower when we use the current generated path to get there.
                     // If it is, update its score and update its parent as well.          
-                    if(adjTile.travelCost > currentPos.travelCost + 1){
+                    if (adjTile.travelCost > currentPos.travelCost + 1) {
                         adjTile.travelCost = currentPos.travelCost + 1;
-                        console.log('A* mechanics: 6.3');
                     }
                 }
             });
         }
 
-        return {success: false, openList, closedList};
+        return { success: false, openList, closedList };
     }
 }
--- js/game.js
+++ js/game.js
@@ -19,56 +19,56 @@ let nextBallColors = [];
 let selectedTile; // tile with selected ball (first click to select, second click to move)
 // let numberOfBusyTiles = 0;
 let numberOfColors;
-let hoveredTile = {x:0, y:0, isPath: false} // only for moving active ball
+let hoveredTile = { x: 0, y: 0, isPath: false } // only for moving active ball
 
-restartGameButton.addEventListener('click', ()=>{
+restartGameButton.addEventListener('click', () => {
     restartGame();
 });
 
-function addNext3Balls(){
+function addNext3Balls() {
 
     const newlyAdded = [];
 
-    for(let i=0; i<3; i++){
-        if(isBoardFull()){
+    for (let i = 0; i < 3; i++) {
+        if (isBoardFull()) {
             return false; // loss
         }
 
-        while(true){ // try to find next free tile in board
+        while (true) { // try to find next free tile in board
             const posX = randomInt(0, 8);
             const posY = randomInt(0, 8);
 
-            if(board[posX][posY] == null){
+            if (board[posX][posY] == null) {
                 const color = nextBallColors[i];
                 const ballNode = document.createElement('div');
                 ballNode.classList.add('ball', 'color' + nextBallColors[i]);
                 tileNodes[posX][posY].appendChild(ballNode);
                 board[posX][posY] = color;
-                newlyAdded.push({posX, posY, color});
+                newlyAdded.push({ posX, posY, color });
                 break;
             }
         }
     }
 
     // for every nextBall: check for 5 in a row after adding it
-    newlyAdded.forEach((ball)=>{
-        checkFor5({x: ball.posX, y: ball.posY}, board, ball.color, boardSize);
+    newlyAdded.forEach((ball) => {
+        checkFor5({ x: ball.posX, y: ball.posY }, board, ball.color, boardSize);
     });
 
-    if(isBoardFull()){
+    if (isBoardFull()) {
         return false; // loss
-    } 
-    
+    }
+
     return true;
 }
 
-function addNewBall(x, y){
+function addNewBall(x, y) {
     const ballElement = document.createElement('div');
     const ballColor = randomInt(1, numberOfColors);
     ballElement.classList.add('ball');
     ballElement.classList.add('color' + ballColor);
     const tile = boardHTMLElement.querySelectorAll(':scope >*')[y].childNodes[x];
-    if(tile.childNodes.length > 0){
+    if (tile.childNodes.length > 0) {
         return false;   // if tile is busy stop function, return false
     }
     tile.appendChild(ballElement);
@@ -77,45 +77,45 @@ function addNewBall(x, y){
     return true;
 }
 
-function checkFor5(movedPos, board, movedColor, boardSize){ // todo: fix, not always working 
+function checkFor5(movedPos, board, movedColor, boardSize) { // todo: fix, not always working 
     // todo: fix vertical, horizontal
-    function checkInDirection(dirX, dirY){
+    function checkInDirection(dirX, dirY) {
 
         let line = [movedPos];
         let iDir = 1; // go in specified by dirX and dirY or direction (1) or go backwards (-1)
         let i = 1;
 
-        for(; true; i+=iDir){ // keep checking for the same color in that direction
-            let x = i*dirX + movedPos.x; // posX
-            let y = i*dirY + movedPos.y; // posY
+        for (; true; i += iDir) { // keep checking for the same color in that direction
+            let x = i * dirX + movedPos.x; // posX
+            let y = i * dirY + movedPos.y; // posY
             // 1. if position is not inside the board:
-            if((x >= 0 && y >= 0 && x < boardSize && y < boardSize) == false){
-                if(iDir == 1){
+            if ((x >= 0 && y >= 0 && x < boardSize && y < boardSize) == false) {
+                if (iDir == 1) {
                     i = 0; // will be -1 after iteration
                     iDir = -1;
                     continue;
                 } else {
-                    if(line.length >= 5){
+                    if (line.length >= 5) {
                         return line;
                     } else {
                         return [];
-                    }    
+                    }
                 }
             }
 
             // 2. if next color is the same, add it to line
-            if(board[x][y] == movedColor){
-                line.push({x, y});  
-            } else  { // no more balls of the same color in this direction, change direction
-                if(iDir == 1){
+            if (board[x][y] == movedColor) {
+                line.push({ x, y });
+            } else { // no more balls of the same color in this direction, change direction
+                if (iDir == 1) {
                     i = 0; // will be -1 after iteration
                     iDir = -1;
                 } else {
-                    if(line.length >= 5){
+                    if (line.length >= 5) {
                         return line;
                     } else {
                         return [];
-                    }    
+                    }
                 }
             }
         }
@@ -127,36 +127,27 @@ function checkFor5(movedPos, board, movedColor, boardSize){ // todo: fix, not al
     let horizontal = checkInDirection(1, 0);
 
     let ballsToRemove = diagonal1.concat(vertical).concat(diagonal2).concat(horizontal);
-    
-    if(ballsToRemove.length > 0){
-        console.log('ballsToRemove', ballsToRemove);
+
+    if (ballsToRemove.length > 0) {
         let removedCount = 0;
-        ballsToRemove.forEach((ball)=>{
-            if(board[ball.x][ball.y] == null){
+        ballsToRemove.forEach((ball) => {
+            if (board[ball.x][ball.y] == null) {
                 return; // skip, it's a duplicate
             }
             board[ball.x][ball.y] = null;
             tileNodes[ball.x][ball.y].innerHTML = '';
             removedCount++;
         });
-        // todo: increase score
         setGameScore(currentScore + removedCount * removedCount);
-
-        console.log('removedCount',removedCount);
-        console.log('ballsToRemove',ballsToRemove);
         return true; // next turn
     } else {
         return false; // no next turn
     }
 }
 
-function checkForLoss(board){
-
-}
-
-function clearPaths(){
-    tileNodes.forEach((row)=>{
-        row.forEach((tile)=>{
+function clearPaths() {
+    tileNodes.forEach(row => {
+        row.forEach(tile => {
             tile.classList.remove('openList');
             tile.classList.remove('closedList');
             tile.classList.remove('finalPath');
@@ -164,25 +155,25 @@ function clearPaths(){
     });
 }
 
-function drawBoard(){
+function drawBoard() {
     // Clear board:
     while (boardHTMLElement.firstChild) {
         boardHTMLElement.removeChild(boardHTMLElement.firstChild);
     }
     board = [];
-    for(let i=0; i<boardSize; i++){
+    for (let i = 0; i < boardSize; i++) {
         board.push([]);
     }
 
     // Draw new rows and tiles:
-    for(let i=0; i<boardSize; i++){
+    for (let i = 0; i < boardSize; i++) {
         const row = document.createElement('div');
         row.classList.add('board__row');
-        for(let j=0; j<boardSize; j++){
+        for (let j = 0; j < boardSize; j++) {
             const tile = document.createElement('div');
             tile.classList.add('board__row__tile');
             tile.addEventListener('click', onTileClick);
-            tile.addEventListener('mouseover', onTileHover);            
+            tile.addEventListener('mouseover', onTileHover);
             row.appendChild(tile);
         }
         boardHTMLElement.appendChild(row);
@@ -190,28 +181,28 @@ function drawBoard(){
     tileNodes = boardToTileNodeArray(boardHTMLElement, boardSize);
 }
 
-function drawPath(from, to){
-    const boardRows = boardHTMLElement.childNodes;
-    let path = aStarFinder.findPath(board, from, to, {x:boardSize, y:boardSize});
+function drawPath(from, to) {
+    let path = aStarFinder.findPath(board, from, to, { x: boardSize, y: boardSize });
+    // uncomment if you want to visualize a-star seraching algorithm:
     // paintPath(path);
 
-    hoveredTile = {x:to.x, y:to.y, isPath: path.success}; 
+    hoveredTile = { x: to.x, y: to.y, isPath: path.success };
 }
 
-function gameOver(){
+function gameOver() {
     alert('Game over. Your score: ' + currentScore);
 }
 
-function isBoardFull(){
-    return board.every((row)=>{
-        return row.every((tile)=>{
+function isBoardFull() {
+    return board.every((row) => {
+        return row.every((tile) => {
             return tile != null
         })
     });
 }
 
-function onTileClick(event){
-    if(isGameOver){
+function onTileClick(event) {
+    if (isGameOver) {
         return;
     }
 
@@ -219,16 +210,16 @@ function onTileClick(event){
 
     let clickedPos = getTilePosition(tile);
 
-    if(board[clickedPos.x][clickedPos.y]){  // if there is ball on clicked tile: change selection
+    if (board[clickedPos.x][clickedPos.y]) {  // if there is ball on clicked tile: change selection
         // remove selection from all balls:
-        document.querySelectorAll('.ball').forEach((el)=>{
+        document.querySelectorAll('.ball').forEach((el) => {
             el.classList.remove('selected');
         });
         // change selected tile, add css class
-        selectedTile = {x: clickedPos.x, y: clickedPos.y, htmlElement: tile};
+        selectedTile = { x: clickedPos.x, y: clickedPos.y, htmlElement: tile };
         selectedTile.htmlElement.children[0].classList.add('selected');
     } else { // if there is no ball on clicked tile
-        if(selectedTile && hoveredTile.isPath){ // move, check if 5 in line
+        if (selectedTile && hoveredTile.isPath) { // move, check if 5 in line
             const clickedTileNode = tileNodes[clickedPos.x][clickedPos.y];
             const selectedTileNode = tileNodes[selectedTile.x][selectedTile.y];
             const selectedBallColor = board[selectedTile.x][selectedTile.y];
@@ -238,33 +229,29 @@ function onTileClick(event){
             board[clickedPos.x][clickedPos.y] = selectedBallColor;
             board[selectedTile.x][selectedTile.y] = null;
             selectedTile = null;
+
             clearPaths();
-            if(checkFor5(clickedPos, board, selectedBallColor, boardSize)){
-                // skip, next turn
-            } else {
-                // addNext3Balls, check for loss
-                if(addNext3Balls() == false){
-                    gameOver();
-                }     
+            if (!checkFor5(clickedPos, board, selectedBallColor, boardSize) && !addNext3Balls()) {
+                gameOver();
             }
             randomNext3Colors();
-        } 
+        }
     }
 }
 
-function onTileHover(event){
+function onTileHover(event) {
     // if there is selected tile and target has no child(tile with no ball) and target is not ball:
-    if(selectedTile && event.target.childNodes.length == 0 && !event.target.classList.contains('ball')){ 
+    if (selectedTile && event.target.childNodes.length == 0 && !event.target.classList.contains('ball')) {
         const tile = getTileFromEventTarget(event.target);
         drawPath(selectedTile, getTilePosition(tile));
     }
 }
 
-function randomNext3Colors(){
+function randomNext3Colors() {
     nextBallColors = [
-        randomInt(1, numberOfColors), 
         randomInt(1, numberOfColors),
-        randomInt(1, numberOfColors)
+        randomInt(1, numberOfColors),
+        randomInt(1, numberOfColors),
     ];
 
     nextBallColorHTMLElements[0].className = '';
@@ -276,7 +263,7 @@ function randomNext3Colors(){
     nextBallColorHTMLElements[2].classList.add('color' + nextBallColors[2], 'nextBallColor');
 }
 
-function restartGame(){
+function restartGame() {
     isGameOver = false;
     boardSize = boardSizeInput.valueAsNumber;
     numberOfColors = numberOfColorsInput.valueAsNumber;
@@ -285,18 +272,18 @@ function restartGame(){
     randomNext3Colors();
     drawBoard();
     // add 3 balls to the board:
-    for(let i=0; i<3; i++){
-        while(true){
-            let x = randomInt(0, boardSize-1);
-            let y = randomInt(0, boardSize-1);
-            if(addNewBall(x,y)){
+    for (let i = 0; i < 3; i++) {
+        while (true) {
+            let x = randomInt(0, boardSize - 1);
+            let y = randomInt(0, boardSize - 1);
+            if (addNewBall(x, y)) {
                 break;
             }
         }
     }
 }
 
-function setGameScore(score){
+function setGameScore(score) {
     currentScore = score;
     currentScoreOutput.innerHTML = score;
 }
