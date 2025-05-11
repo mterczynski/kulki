44a1b0a08162f43abb2260b01c642cc4cb3b85ab
Michał Terczyński <mterczynski1@gmail.com>
2025-05-12 00:24:55 +0200

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+# 1. Rebuild
+npm i;
+npm run build;
+# 2. Update build in ../mterczynski.github.io
+rm -rf ../mterczynski.github.io/kulki
+mkdir ../mterczynski.github.io/kulki
+cp -r ./dist/* ../mterczynski.github.io/kulki/
+cd ../mterczynski.github.io/kulki
+
+
+# 3. Commit in ../mterczynski.github.io
+git add .
+git commit -a -m "Update kulki build"
+# 4. Push in ../mterczynski.github.io
+git push
